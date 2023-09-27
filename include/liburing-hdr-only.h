8526c7eb99036771bacc829cae9c3a0d81e65356
Mohammad Bahoosh <12122474+the-moisrex@users.noreply.github.com>
2023-09-27 03:21:10 -0800

    header-only header

---
+++ include/liburing-hdr-only.h
@@ -0,0 +1,10 @@
+// Created by moisrex on 9/27/23.
+
+#ifndef LIBURING_HDR_ONLY_LIBURING_HDR_ONLY_H
+#define LIBURING_HDR_ONLY_LIBURING_HDR_ONLY_H
+
+/// the purpose of this file is for anyone trying to have a way to
+/// distinguish between the liburing.h and the header only version
+#include "./liburing.h"
+
+#endif // LIBURING_HDR_ONLY_LIBURING_HDR_ONLY_H
